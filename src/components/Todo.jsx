dbc43f02b587fa56be26c8db4c6611d513d8c8ea
Harsh Gupta <guptaharsh728@gmail.com>
2024-07-17 00:57:44 +0530

    media querry updated

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -57,9 +57,9 @@ function Todo() {
         </div>
 
         {/* -----------------Input-Box--------------- */}
-        <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        <div className='flex items-center w-full my-7 bg-gray-200 rounded-full'>
             <input ref={inputRef} className='bg-transparent border-none outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-24 sm:w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
         </div>
 
         {/* -----------------Todo-List--------------- */}
